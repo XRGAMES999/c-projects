19f856a1df48a2d4a8d017c2df6c88c717901bc4
Ahmed Naiser <52338442+XRGAMES999@users.noreply.github.com>
2023-11-25 20:47:06 +0300

    Add files via upload

---
+++ (1) + (1+2) + (1+2+3).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int sum=0,i,j,n;
+cout<<"enter n"<<endl;
+cin>>n;
+for ( i = 1; i <=n; i++)
+{
+   for ( j = 1; j <=i; j++)
+   
+   {sum+=j;
+   
+   
+}}
+cout<<"sum"<<sum<<endl;
+return 0;
+}
